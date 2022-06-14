4dd94b0aeb469090717af99db68e6d4791270cba
hamlet10 <hpsanchez@live.com>
2022-06-14 19:24:28 -0400

    course information, step1

---
+++ part1/courseinfo/src/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+
+    console.log('header')
+    return (
+        <div>
+            <header>
+                <h1>{props.course}</h1>
+            </ header>
+        </div>
+    )
+}
+
+export default Header;
