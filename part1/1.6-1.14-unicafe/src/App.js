11e8e924f84e2d22dbc02778648a6d76dc711740
hamlet10 <hpsanchez@live.com>
2023-01-26 16:17:02 -0400

    Revert "1.7: unicafe step2"
    This reverts commit f62c452388e9b42dfd59317c28ddbb14f42a6ce4.

--- part1/1.6-1.14-unicafe/src/App.js
+++ part1/1.6-1.14-unicafe/src/App.js
@@ -1,46 +1,32 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const Button = (props) => {
-  console.log(props);
-  return <button onClick={props.handelClick}>{props.text}</button>;
-};
+  console.log(props)
+  return(
+
+    <button onClick={props.handelClick}>{props.text}</button>
+    
+  );
+}
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
-
-  const handelGood = () => {
-    setAll(all + 1);
-    setGood(good + 1);
-  };
-
-  const handelNeultral = () => {
-    setAll(all + 1);
-    setNeutral(neutral + 1);
-  };
-
-  const hanelBad = () => {
-    setAll(all + 1);
-    setBad(bad + 1);
-  };
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handelClick={handelGood} text="good" />
-      <Button handelClick={handelNeultral} text="neutral" />
-      <Button handelClick={hanelBad} text="bad" />
+      <Button handelClick={() => setGood(good + 1)} text='good'/>
+      <Button handelClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button handelClick={() => setBad(bad + 1)} text='bad'/>
       <h1>Statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {(good - bad) / all} </div>
+      <div>good {good}</div>   
+      <div>neutral {neutral}</div>   
+      <div>bad {bad}</div>   
     </div>
-  );
-};
+  )
+}
 
 export default App;
