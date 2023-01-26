493e2fb4c593f7be6145ac56aee4bd3cd60e023c
hamlet10 <hpsanchez@live.com>
2023-01-26 16:41:10 -0400

    1.7: unicafe step2

--- part1/1.6-1.14-unicafe/src/App.js
+++ part1/1.6-1.14-unicafe/src/App.js
@@ -14,17 +14,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handelGood =() =>{
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+
+  const handelNeutral = () =>{
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
+
+  const handelBad = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handelClick={() => setGood(good + 1)} text='good'/>
-      <Button handelClick={() => setNeutral(neutral + 1)} text='neutral'/>
-      <Button handelClick={() => setBad(bad + 1)} text='bad'/>
+      <Button handelClick={handelGood} text='good'/>
+      <Button handelClick={handelNeutral} text='neutral'/>
+      <Button handelClick={handelBad} text='bad'/>
       <h1>Statistics</h1>
       <div>good {good}</div>   
       <div>neutral {neutral}</div>   
       <div>bad {bad}</div>   
+      <div>all {all}</div>   
+      <div>average {isNaN((good - bad) / all)? 0 : (good - bad) / all}</div>   
+      <div>positive {~~((good / all) * 100)}%</div>   
     </div>
   )
 }
