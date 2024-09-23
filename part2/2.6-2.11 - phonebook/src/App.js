973e7dbbde3262c043663f307fb468a97a38c217
hamlet10 <hpsanchez@live.com>
2024-09-23 11:35:38 -0400

    2.12: The Phonebook step 7

--- part2/2.6-2.11 - phonebook/src/App.js
+++ part2/2.6-2.11 - phonebook/src/App.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import personService from './services.js/person';
 import Persons from "./components/Persons"
 import Form from "./components/Form";
 import Filter from "./components/Filter";
+import person from './services.js/person'
 
 const App = () => {
   const [persons, setPersons] = useState([]);
 
   useEffect(() =>{
-    axios.get('http://localhost:3001/persons')
-    .then(response => setPersons(response.data))
+    personService.getAll()
+    .then(response => {setPersons(response.data)})
+    // axios.get('http://localhost:3001/persons')
+    // .then(response => setPersons(response.data))
   },[])
 
 
@@ -22,7 +25,11 @@ const App = () => {
       alert(`${NuPerson.name} ya esta en la lista`);
     } else {
       const newCotact = NuPerson
-      setPersons(persons.concat(newCotact));
+      personService.create(NuPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
+      // setPersons(persons.concat(newCotact));
     }
     console.log(persons);
   };
